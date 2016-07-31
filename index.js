7948082e9cbdd5853fba79e3ce0faf2267ba6bd5
nightire <nightire@very-geek.com>
2016-08-01 00:14:20 +0800

    🍯 Improve compatibility as nested addon

--- index.js
+++ index.js
@@ -3,20 +3,22 @@
 module.exports = {
   name: 'ember-refined-remarkable',
 
-  included(app) {
-    this._super.included.apply(this, ...arguments);
+  included(app, parentAddon) {
+    const target = parentAddon || app;
+    this._super.included.call(this, target);
 
-    this.app.import(app.bowerDirectory + '/remarkable/dist/remarkable.js');
-    this.app.import('vendor/shims/remarkable.js');
+    const bowerDir = target.bowerDirectory;
 
-    this.app.import(app.bowerDirectory + '/highlightjs/highlight.pack.js');
-    this.app.import('vendor/shims/highlight.js');
+    target.import(bowerDir + '/remarkable/dist/remarkable.js');
+    target.import('vendor/shims/remarkable.js');
 
-    const addonOptions = app.options[this.name];
+    target.import(bowerDir + '/highlightjs/highlight.pack.js');
+    target.import('vendor/shims/highlight.js');
 
-    if (addonOptions && addonOptions.theme) {
-      this.app.import(app.bowerDirectory
-        + '/highlightjs/styles/' + addonOptions.theme + '.css');
+    const options = target.options[this.name];
+
+    if (options && options.theme) {
+      target.import(bowerDir + '/highlightjs/styles/' + options.theme + '.css');
     }
   }
 };
